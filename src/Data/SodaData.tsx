a49b1979d5afcb6404d2d01cf6e4c320f0536b72
NZpatelK <patelk.dev@hotmail.com>
2023-11-09 20:08:12 +1300

    Update new design and can and fix the layout

--- src/Data/SodaData.tsx
+++ src/Data/SodaData.tsx
@@ -1,7 +1,15 @@
 import mangoImg  from '../assets/MongoSodaCan.png';
-import watermelonImg from '../assets/watermelonSodaCan.png';
+import watermelonImg from '../assets/watermeloncan.png';
 import mangoBg from '../assets/mangoBG.png'
 import watermelonBg from '../assets/ware.png'
+import aproctimg from '../assets/apriocctcan.png'
+import aproctbg from '../assets/apriocctBg.png'
+import blueberryimg from '../assets/blueberrycan.png'
+import blueberrybg from '../assets/blueberrybg.png'
+import graphfriuitImg from '../assets/graphfruitcan.png'
+import graphfruitbg from '../assets/graphfruitbg.png'
+import pemoImg from '../assets/pomecan.png'
+import pemoBg from '../assets/pomebg.png'
 
 export const SodaData = [
     {
@@ -25,5 +33,49 @@ export const SodaData = [
         bgImg: watermelonBg,
         imgTransitionStatus:'sodaImgExit',
         textTransitionStatus:'sodaTextDownExit'
-    }
+    },
+    {
+        id:1,
+        title: 'Apricot Euphoria Fizz',
+        breif: 'Apricot Euphoria Fizz is a apricot flavored soda that is made with real apricot juice. It is a refreshing drink that is perfect for a hot summer day. ',
+        price: 1.99,
+        bgColour: '#ff0000',
+        img: aproctimg,
+        bgImg: aproctbg,
+        imgTransitionStatus:'sodaImgExit',
+        textTransitionStatus:'sodaTextDownExit'
+    },
+    {
+        id:1,
+        title: 'Blueberry Bubby burst',
+        breif: 'Blueberry bubby burst is a blueberry flavored soda that is made with real blueberry juice. It is a refreshing drink that is perfect for a hot summer day.',
+        price: 1.99,
+        bgColour: '#ff0000',
+        img: blueberryimg,
+        bgImg: blueberrybg,
+        imgTransitionStatus:'sodaImgExit',
+        textTransitionStatus:'sodaTextDownExit'
+    },
+    {
+        id:1,
+        title: 'Grapefruit Zest Zing',
+        breif: 'Graperfruit Zest Zing is a grapefruit flavored soda that is made with real grapefruit juice. It is a refreshing drink that is perfect for a hot summer day.',
+        price: 1.99,
+        bgColour: '#ff0000',
+        img: graphfriuitImg,
+        bgImg: graphfruitbg,
+        imgTransitionStatus:'sodaImgExit',
+        textTransitionStatus:'sodaTextDownExit'
+    },
+    {
+        id:1,
+        title: 'Pomegranate Paradise Punch',
+        breif: 'Pomegranate Paradise Punch is a pomegranate flavored soda that is made with real pomegranate juice. It is a refreshing drink that is perfect for a hot summer day.',
+        price: 1.99,
+        bgColour: '#ff0000',
+        img: pemoImg,
+        bgImg: pemoBg,
+        imgTransitionStatus:'sodaImgExit',
+        textTransitionStatus:'sodaTextDownExit'
+    },
 ]
